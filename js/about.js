7abad3141a947bfac7332cb075455c1c4c412657
Genesis Flores <floresgk@uci.edu>
2025-10-26 02:25:43 -0700

    about.js formatting dates
    Separated tweet array solely with their dates. Then got the max and min of the dates. Made a function to format the dates in the needed way. Then used that and DOM manipulated it.

--- js/about.js
+++ js/about.js
@@ -11,7 +11,32 @@ function parseTweets(runkeeper_tweets) {
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
-	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('numberTweets').innerText = tweet_array.length;
+	
+	// getting all of the tweet dates
+	let dates = tweet_array.map(tweet => tweet.time);
+	
+	// getting the earliest and latest date from all the dates
+	let earliest_date = new Date(Math.min(...dates));
+	let latest_date = new Date(Math.max(...dates));
+
+
+	// making the dates for the about page formatted into proper way
+	function format_date(tweet_date) {
+		return tweet_date.toLocaleDateString('en-US', {
+		year: "numeric",
+		month: "long",
+		day: "numeric"
+	});
+	};
+	
+	// actual formatted dates 
+	const FORMATTED_EARLY_DATE = format_date(earliest_date);
+	const FORMATTED_LATE_DATE = format_date(latest_date);
+
+	// dom manipulating for actual dates formatted
+	document.getElementById('firstDate').innerText = FORMATTED_EARLY_DATE;
+	document.getElementById('lastDate').innerText = FORMATTED_LATE_DATE;
 }
 
 //Wait for the DOM to load
