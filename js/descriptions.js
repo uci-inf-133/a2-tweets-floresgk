44d3d22fe4669b6a0403e1cd8ecd3cb6851f5722
Genesis Flores <floresgk@uci.edu>
2025-10-31 19:08:52 -0700

    Update descriptions.js
    Get the tweet array with the runkeeper, then the making it so that it is only including the written tweets.  And adding to event handler, make the ids from html, and then making the event listeners to clear the table and other data. additionally, using the tweet array that only has written tweets to match and then use it to update table.

--- js/descriptions.js
+++ js/descriptions.js
@@ -4,13 +4,54 @@ function parseTweets(runkeeper_tweets) {
 		window.alert('No tweets returned');
 		return;
 	}
+	
+	tweet_array = runkeeper_tweets.map(function(tweet) {
+		return new Tweet(tweet.text, tweet.created_at);
+	});
 
 	//TODO: Filter to just the written tweets
+	
+	tweet_array = runkeeper_tweets.map(tweet => new Tweet(tweet.text, tweet.created_at));
+	tweet_array = tweet_array.filter(tweet => tweet.writtenText && tweet.writtenText.length > 0);
+   
+
+
 }
+
 
 function addEventHandlerForSearch() {
 	//TODO: Search the written tweets as text is entered into the search box, and add them to the table
+	const searchInp = document.getElementById("textFilter"); 
+	const tableBody = document.getElementById("tweetTable"); 
+	const searchCountSpan = document.getElementById("searchCount");
+	const searchTextSpan = document.getElementById("searchText");
+
+	searchInp.addEventListener("input", () => {
+		const searchTerm = searchInp.value.trim().toLowerCase();
+		// clear table and update count if empty search
+		if (searchTerm === "") {
+			tableBody.innerHTML = "";
+			searchCountSpan.innerText = 0;
+			searchTextSpan.innerText = "";
+			return;
+		}
+		// matching written tweets to search term
+		const filteredTweets = tweet_array.filter(tweet => 
+			tweet.writtenText.toLowerCase().includes(searchTerm)
+		);
+		// getting the new term and number of tweets updated
+		searchCountSpan.innerText = filteredTweets.length;
+		searchTextSpan.innerText = searchTerm;
+		tableBody.innerHTML = "";
+
+		filteredTweets.forEach((tweet, index) => {
+			const rowHTML = tweet.getHTMLTableRow(index + 1);
+			tableBody.insertAdjacentHTML('beforeend', rowHTML);
+		})
+	});
 }
+
+
 
 //Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
